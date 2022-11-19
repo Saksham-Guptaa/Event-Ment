e2ee79e3e5b5d35cd3da9edc8ea10a8a70a31245
Saksham Gupta <creator.sakshamgupta@gmail.com>
2022-11-19 20:28:24 +0530

    initial commit

---
+++ pages/search.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+function Search() {
+  return (
+    <div >
+        <Header />
+
+        <main className='flex'>
+            <section>
+                <p>300+ Farms</p>
+            </section>
+
+        </main>
+
+        <Footer />
+    </div>
+  )
+}
+
+export default Search
